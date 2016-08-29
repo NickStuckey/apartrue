8051fc36578a3961fbd81de5d49f0cdcb7981dc1
Sam Stuckey <sam@Samuels-Air.lan>
2016-08-28 21:55:21 -0400

    Fix neighborhood id

--- frontend/components/property_map.jsx
+++ frontend/components/property_map.jsx
@@ -73,7 +73,7 @@ const PropertyMap = React.createClass ({
     const newProperties = PropertyStore.all();
     const newPropertyIds = Object.keys(newProperties);
     // get the new neighborhood from the a property in the hash
-    const hoodId = newProperties[newPropertyIds[0]].neighborhoodId;
+    // const hoodId = newProperties[newPropertyIds[0]].neighborhood_id;
     // this.updateMapCenter(hoodId);
 
     let properties = [];
