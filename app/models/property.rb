ac8b729ab536c53ed18dded3248303dac763be75
SamStuckey <sam.n.stuckey@gmail.com>
2016-08-30 21:39:11 -0400

    Refactor satisfy_params method of properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -16,7 +16,11 @@ class Property < ActiveRecord::Base
   )
 
   def self.satisfy_params(filters)
-    properties = self.where(lat:filters[:bounds][:southWest][:lat]...filters[:bounds][:northEast][:lat],lng: filters[:bounds][:southWest][:lng]...filters[:bounds][:northEast][:lng]).where('price < ?', filters[:price]).where('num_bedrooms = ?', filters[:num_bedrooms])
+    properties = self
+    .where(lat:filters[:bounds][:southWest][:lat]...filters[:bounds][:northEast][:lat],lng: filters[:bounds][:southWest][:lng]...filters[:bounds][:northEast][:lng])
+    .where('price < ?', filters[:price])
+    .where('num_bedrooms = ?', filters[:num_bedrooms])
+    .where(neighborhood_id: filters[:neighborhood_id])
     properties
   end
 
